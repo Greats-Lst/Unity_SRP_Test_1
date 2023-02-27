d7a4d5a34e7e490a924f4e42a25b65878fefb71d
LiShaote <651289027@qq.com>
2023-02-27 22:15:54 +0800

    add:Charpter1.3.4 Drawing Gizmos（较明显的是摄像机的Gizmos）

--- Assets/Custom RP/Runtime/CameraRender/CommonCameraRender.Editor.cs
+++ Assets/Custom RP/Runtime/CameraRender/CommonCameraRender.Editor.cs
@@ -7,37 +7,48 @@ partial class CommonCameraRender
 {
     partial void DrawUnsupportedShaders();
 
+    partial void DrawGizmos();
+
 #if UNITY_EDITOR
-        private static ShaderTagId[] m_legacy_shader_tag_ids =
+    private static ShaderTagId[] m_legacy_shader_tag_ids =
+    {
+        new ShaderTagId("Always"),
+        new ShaderTagId("ForwardBase"),
+        new ShaderTagId("PrepassBase"),
+        new ShaderTagId("Vertex"),
+        new ShaderTagId("VertexLMRGBM"),
+        new ShaderTagId("VertexLM")
+    };
+
+    // Editor Rendering
+    private static Material m_error_mat;
+
+    partial void DrawUnsupportedShaders()
+    {
+        if (m_error_mat == null)
         {
-            new ShaderTagId("Always"),
-            new ShaderTagId("ForwardBase"),
-            new ShaderTagId("PrepassBase"),
-            new ShaderTagId("Vertex"),
-            new ShaderTagId("VertexLMRGBM"),
-            new ShaderTagId("VertexLM")
-        };
-
-        // Editor Rendering
-        private static Material m_error_mat;
-
-        partial void DrawUnsupportedShaders()
-        {
-            if (m_error_mat == null)
-            {
-                m_error_mat = new Material(Shader.Find("Hidden/InternalErrorShader"));
-            }
-
-            var drawing_setting = new DrawingSettings(m_legacy_shader_tag_ids[0], new SortingSettings(m_camera));
-            drawing_setting.overrideMaterial = m_error_mat;
-            var filtering_setting = FilteringSettings.defaultValue;
-
-            for (int i = 1; i < m_legacy_shader_tag_ids.Length; ++i)
-            {
-                drawing_setting.SetShaderPassName(i, m_legacy_shader_tag_ids[i]);
-            }
-
-            m_context.DrawRenderers(m_cull_res, ref drawing_setting, ref filtering_setting);
+            m_error_mat = new Material(Shader.Find("Hidden/InternalErrorShader"));
         }
+
+        var drawing_setting = new DrawingSettings(m_legacy_shader_tag_ids[0], new SortingSettings(m_camera));
+        drawing_setting.overrideMaterial = m_error_mat;
+        var filtering_setting = FilteringSettings.defaultValue;
+
+        for (int i = 1; i < m_legacy_shader_tag_ids.Length; ++i)
+        {
+            drawing_setting.SetShaderPassName(i, m_legacy_shader_tag_ids[i]);
+        }
+
+        m_context.DrawRenderers(m_cull_res, ref drawing_setting, ref filtering_setting);
+    }
+
+    partial void DrawGizmos()
+    {
+        if (UnityEditor.Handles.ShouldRenderGizmos())
+        {
+            m_context.DrawGizmos(m_camera, GizmoSubset.PreImageEffects);
+            m_context.DrawGizmos(m_camera, GizmoSubset.PostImageEffects);
+        }
+    }
 #endif
 }
--- Assets/Custom RP/Runtime/CameraRender/CommonCameraRender.cs
+++ Assets/Custom RP/Runtime/CameraRender/CommonCameraRender.cs
@@ -33,6 +33,7 @@ public partial class CommonCameraRender
         Setup();
         DrawVisibleGeometry();
         DrawUnsupportedShaders();
+        DrawGizmos();
         Submit();
     }
 
