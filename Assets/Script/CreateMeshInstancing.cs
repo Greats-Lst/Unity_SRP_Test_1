21b51058cb30557125bdb69254080699ed5365b3
LiShaote <651289027@qq.com>
2023-03-11 15:42:48 +0800

    add:Charpter3.3.10 更新了随机生成Lit Shader材质球体的脚本

--- Assets/Script/CreateMeshInstancing.cs
+++ Assets/Script/CreateMeshInstancing.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CreateMeshInstancing : MonoBehaviour
 {
     private static int m_color_id = Shader.PropertyToID("_BaseColor");
+    private static int m_metalic_id = Shader.PropertyToID("_Metalic");
+    private static int m_smoothness_id = Shader.PropertyToID("_Smoothness");
 
     [SerializeField]
     private Mesh m_mesh;
@@ -17,6 +19,8 @@ public class CreateMeshInstancing : MonoBehaviour
 
     private Matrix4x4[] m_trs;
     private Vector4[] m_colors;
+    private float[] m_matelics;
+    private float[] m_smoothness;
 
     private MaterialPropertyBlock m_block;
 
@@ -24,6 +28,8 @@ public class CreateMeshInstancing : MonoBehaviour
     {
         m_trs = new Matrix4x4[m_mesh_count];
         m_colors = new Vector4[m_mesh_count];
+        m_matelics = new float[m_mesh_count];
+        m_smoothness = new float[m_mesh_count];
 
         for (int i = 0; i < m_trs.Length; ++i)
         {
@@ -32,6 +38,8 @@ public class CreateMeshInstancing : MonoBehaviour
                 new Vector3(Random.Range(0.5f, 1.5f), Random.Range(0.5f, 1.5f), Random.Range(0.5f, 1.5f)));
 
             m_colors[i] = new Vector4(Random.value, Random.value, Random.value, Random.Range(0.5f, 1.0f));
+            m_matelics[i] = Random.value < 0.25 ? 1f : 0f;
+            m_smoothness[i] = Random.Range(0.05f, 0.95f);
         }
     }
 
@@ -41,6 +49,8 @@ public class CreateMeshInstancing : MonoBehaviour
         {
             m_block = new MaterialPropertyBlock();
             m_block.SetVectorArray(m_color_id, m_colors);
+            m_block.SetFloatArray(m_metalic_id, m_matelics);
+            m_block.SetFloatArray(m_smoothness_id, m_smoothness);
         }
 
         Graphics.DrawMeshInstanced(m_mesh, 0, m_mat, m_trs, m_mesh_count, m_block);
