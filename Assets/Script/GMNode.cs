33c1e7fadba989c62e17f1a996b2a4c342ef0ae8
LiShaote <651289027@qq.com>
2023-03-21 22:09:47 +0800

    ---------- End of Charpter 4 ----------

---
+++ Assets/Script/GMNode.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GMNode : MonoBehaviour
+{
+    private float x = 10000000.9f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.LogError(x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
