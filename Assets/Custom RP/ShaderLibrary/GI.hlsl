cc427ff096b668ccc06b700b66dcf2f0beb32f4a
LiShaote <651289027@qq.com>
2023-04-01 11:41:19 +0800

    add:Chapter5 Sample Bake Light

--- Assets/Custom RP/ShaderLibrary/GI.hlsl
+++ Assets/Custom RP/ShaderLibrary/GI.hlsl
@@ -1,10 +1,15 @@
 #ifndef CUSTOM_GI_INCLUDE
 #define CUSTOM_GI_INCLUDE
 
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/EntityLighting.hlsl"
+
+TEXTURE2D(unity_Lightmap); // unity的烘培光照应该就在这里了
+SAMPLER(samplerunity_Lightmap);
+
 #if defined (LIGHTMAP_ON)
 	#define GI_ATTRIBUTE_DATA float2 lightMapUV : TEXCOORD1; // The light map UV are provided via the second texture coordinates channel
 	#define GI_VARYINGS_DATA float2 lightMapUV : VAR_LIGHT_MAP_UV;
-	#define TRANSFER_GI_DATA(input, output)  output.lightMapUV = input.lightMapUV;
+	#define TRANSFER_GI_DATA(input, output)  output.lightMapUV = input.lightMapUV * unity_LightmapST.xy + unity_LightmapST.zw;
 	#define GI_FRAGMENT_DATA(input) input.lightMapUV
 #else
 	#define GI_ATTRIBUTE_DATA
@@ -18,10 +23,26 @@ struct GI
 	float3 diffuse;
 };
 
+float3 SampleLightMap(float2 lightmap_uv)
+{
+#if defined(LIGHTMAP_ON)
+	return SampleSingleLightmap(TEXTURE2D_ARGS(unity_Lightmap, samplerunity_Lightmap),
+		lightmap_uv, float4(1.0, 1.0, 0.0, 0.0), 
+	#if defined(UNITY_LIGHTMAP_FULL_HDR)
+		false,
+	#else 
+		true,
+	#endif
+		float4(LIGHTMAP_HDR_MULTIPLIER, LIGHTMAP_HDR_EXPONENT, 0.0, 0.0));
+#else
+	return 0;
+#endif
+}
+
 GI GetGI(float2 lightmap_uv)
 {
 	GI gi;
-	gi.diffuse = float3(lightmap_uv, 0.0);
+	gi.diffuse = SampleLightMap(lightmap_uv);
 	return gi;
 }
 
--- Assets/Custom RP/ShaderLibrary/UnityInput.hlsl
+++ Assets/Custom RP/ShaderLibrary/UnityInput.hlsl
@@ -1,12 +1,16 @@
 #ifndef CUSTOM_UNITY_INPUT_INCLUDE
 #define CUSTOM_UNITY_INPUT_INCLUDE
 
+// All UnityPerDraw data gets instanced when needed.
 CBUFFER_START(UnityPerDraw)
 	float4x4 unity_ObjectToWorld;
 	float4x4 unity_WorldToObject;
 	float4 unity_LODFade;
 	real4 unity_WorldTransformParams;
 	float3 _WorldSpaceCameraPos;	// 该变量的位置是我自己放的 我感觉可以放进这里，因为每次DrawCall都重新计算？
+
+	float4 unity_LightmapST;
+	float4 unity_DynamicLightmapST;
 CBUFFER_END
 
 
